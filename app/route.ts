63fe8b85ff5013a1f451818c81f2e2740750948c
mikeyw48 <miwyciszk@gmail.com>
2026-04-02 13:48:21 -0500

    GET function with  hosting and asn lookups

--- app/route.ts
+++ app/route.ts
@@ -1,5 +1,33 @@
-import { NextResponse } from "next/server";
+import { getASN } from "@/lib/asn";
+import { getHosting } from "@/lib/hosting";
+import { NextRequest, NextResponse } from "next/server";
+import z from "zod";
 
-export async function GET() {
-  return NextResponse.json({ message: "Hello world!" });
+const ipSchema = z.ipv4().or(z.ipv6());
+
+export async function GET(request: NextRequest) {
+  const searchParams = request.nextUrl.searchParams;
+  const query = searchParams.get("query");
+
+  const result = ipSchema.safeParse(query);
+
+  if (!result.success) {
+    return NextResponse.json({ error: "Invalid IP Address" }, { status: 400 });
+  }
+
+  const asnResult = getASN(result.data);
+
+  if (!asnResult.success) {
+    return NextResponse.json({ error: asnResult.error }, { status: 404 });
+  }
+
+  const isHosting = await getHosting(asnResult.asn!);
+
+  return NextResponse.json({
+    success: asnResult.success,
+    ip: result.data,
+    asn: asnResult.asn,
+    org: asnResult.org,
+    isHosting: isHosting.isHosting
+  });
 }
