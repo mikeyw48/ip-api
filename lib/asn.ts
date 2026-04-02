faa2484673b4f60183e3812e252b382b971e7677
mikeyw48 <miwyciszk@gmail.com>
2026-04-02 13:47:54 -0500

    ASN Lookup function

---
+++ lib/asn.ts
@@ -0,0 +1,28 @@
+import maxmind, { AsnResponse } from "maxmind";
+
+const lookup = await maxmind.open<AsnResponse>("./data/geolite2-asn.mmdb");
+
+export function getASN(query: string) {
+  try {
+    const result = lookup.get(query);
+
+    if (!result){
+      return {
+        success: false,
+        error: "No ASN found for IP"
+      }
+    }
+
+    return {
+      success: true,
+      asn: result.autonomous_system_number,
+      org: result.autonomous_system_organization
+    }
+
+  } catch (err) {
+    return {
+      success: false,
+      error: "Invalid IP or ASN lookup failure"
+    }
+  }
+}
